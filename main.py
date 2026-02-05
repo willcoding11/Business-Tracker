0ad0fe72651fd7280174a6da563d1f6c5ee12c03
Will Culver <will.coding11@gmail.com>
2026-02-05 12:22:25 -0800

    Display

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("AI Business Tracker")
+
+BG = (189, 244, 255) # Light blue
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    window.fill(BG)
+
+    pygame.display.flip()
+
+pygame.quit()
